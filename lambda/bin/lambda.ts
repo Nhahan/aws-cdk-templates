3afb2c6703be15f1fd0a753e284c9bbb5306bdf7
sun <sun@codebrick.co>
2022-02-24 01:20:07 +0900

    lambda templates

--- lambda/bin/lambda.ts
+++ lambda/bin/lambda.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LambdaStack } from '../lib/lambda-stack';
+import { LambdaStack } from '../lib/stacks/lambda-stack';
 
 const app = new cdk.App();
 new LambdaStack(app, 'LambdaStack', {
